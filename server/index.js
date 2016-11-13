413eba8e767fd8d73fbf17935faa7c7f2c8085c3
Sam <samcackett@gmail.com>
2016-11-13 16:13:01 +0000

    Add basic express server to receive contact form

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-app.use(express.static('frontend'));
+// create application/json parser
+var jsonParser = bodyParser.json();
+
+app.use(express.static('../frontend'));
+
+app.post('/email', jsonParser, function(req, res){
+  console.log('body: ' + JSON.stringify(req.body));
+  res.send();
+});
 
 app.listen(3000, function () {
   console.log('Listening on port 3000');
